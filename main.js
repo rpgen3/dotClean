9a2ed1d7dfb67814ed08216b96c2b994a6453d3c
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-06-19 20:43:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -24,6 +24,12 @@
         color: isError ? 'red' : 'blue',
         backgroundColor: isError ? 'pink' : 'lightblue'
     });
+    $('<button>').appendTo(h).text('処理').on('click', ()=>main());
+    const inputNoise = rpgen3.addSelect(h,{
+        label: 'ノイズ除去度',
+        list: [2, 3, 4],
+        value: 3
+    });
     $('<input>').appendTo(h).prop({
         type: "file"
     }).on('change',e => {
@@ -31,7 +37,6 @@
     });
     const inputImg = rpgen3.addInputStr(h,{
         label: '画像URL入力',
-        save: true,
         value: 'https://i.imgur.com/MrOrXaY.png'
     });
     inputImg.elm.on('change', () => {
@@ -41,16 +46,25 @@
         crossOrigin: "anonymous"
     });
     inputImg.elm.trigger('change');
-    $('<button>').appendTo(h).text('処理').on('click', ()=>main());
     const output = $('<div>').appendTo(h);
-    const main = () => {
+    const sleep = ms => new Promise(resolve=>setTimeout(resolve, ms));
+    const dialog = async str => {
+        msg(str);
+        await sleep(100);
+    };
+    const main = async () => {
         const img = imgElm.get(0),
               {width, height} = img,
               cv = $('<canvas>').prop({width, height}),
               ctx = cv.get(0).getContext('2d');
         ctx.drawImage(img, 0, 0);
         const {data} = ctx.getImageData(0, 0, width, height);
-        return toCv(laplacian(data, width, height), width, height);
+        await dialog('エッジ検出します');
+        const bin = laplacian(data, width, height);
+        await dialog('エッジ検出完了。ノイズを取り除きます');
+        cleanBin(bin, width, height);
+        await dialog('ノイズを取り除きました');
+        return toCv(binToCv(bin), width, height);
         const colors = modeColors(toJoin(mass).flat());
         for(const a of mass){
             for(const b of a){
@@ -60,17 +74,10 @@
         const unit = 2;
         makeCanvas(toJoin(mass), unit).appendTo(output.empty());
     };
-    const toJoin = mass => mass.map(v=>v.map(v=>v.join('#')));
-    ////////////////////////////////////////////////////////////////////////////
-    const luminance = (()=>{
-        const r = 0.298912,
-              g = 0.586611,
-              b = 0.114478;
-        return RGB => r * RGB[0] + g * RGB[1] + b * RGB[2] | 0;
-    })();
+    const luminance = ([r,g,b]) => r * 0.298912 + g * 0.586611 + b * 0.114478 | 0;
     const laplacian = (data, w, h) => {
-        const index = (x,y) => x + y * h << 2,
-              data2 = data.slice(),
+        const index = (x,y) => x + y * w,
+              d = new Uint8ClampedArray(data.length >> 2),
               kernel = [
                   0, 0, 1, 0, 0,
                   0, 1, 2, 1, 0,
@@ -82,21 +89,41 @@
               p = size >> 1;
         for(let i = 0; i < data.length; i += 4){
             const x = (i >> 2) % w,
-                  y = (i >> 2) / w | 0,
-                  rgb = data.slice(i, i + 3);
+                  y = (i >> 2) / w | 0;
+            if(x < p || y < p || x >= w - p || y >= h - p) continue;
+            const rgb = data.slice(i, i + 3);
             let sum = 0;
             for (const [i,v] of kernel.entries()) {
                 const xx = i % size,
                       yy = i / size | 0,
-                      j = index(x + xx, y + yy);
-                sum += luminance(data.slice(j, j + 3)) * v;
+                      j = index(x + xx - p, y + yy - p) << 2;
+                sum += v * luminance(data.slice(j, j + 3));
             }
-            const j = index(x,y);
-            data2[j] = data2[j + 1] = data2[j + 2] = 255 - (sum < 0 ? 0 : sum > 255 ? 255 : sum);
+            if(0x80 < sum) d[index(x,y)] = 1;
         }
-        return data2;
+        return d;
     };
-    ////////////////////////////////////////////////////////////////////////////
+    const cleanBin = (bin, w, h) => {
+        const index = (x,y) => x + y * w,
+              p = 1,
+              noise = inputNoise();
+        for(const [i,v] of bin.entries()){
+            if(!v) continue;
+            const x = i % w,
+                  y = i / w | 0;
+            if(x < p || y < p || x >= w - p || y >= h - p) continue;
+            const j = index(x - 1, y - 1),
+                  k = index(x - 1, y),
+                  l = index(x - 1, y + 1),
+                  sum = [
+                      ...bin.slice(j, j + 3),
+                      bin[k],
+                      bin[k + 2],
+                      ...bin.slice(l, l + 3)
+                  ].reduce((p,x) => p + x);
+            if(sum < noise) bin[index(x, y)] = 0;
+        }
+    };
     const count = arr => {
         const map = new Map();
         for(const v of arr) map.set(v, map.has(v) ? map.get(v) + 1 : 1);
@@ -122,5 +149,14 @@
               ctx = cv.get(0).getContext('2d');
         ctx.putImageData(new ImageData(data, width, height), 0, 0);
         return cv.appendTo(output.empty());
+    };
+    const binToCv = bin => {
+        const d = new Uint8ClampedArray(bin.length << 2);
+        for(const [i,v] of bin.entries()){
+            const j = i << 2;
+            if(!v) d[j] = d[j + 1] = d[j + 2] = 255;
+            d[j + 3] = 255;
+        }
+        return d;
     };
 })();
